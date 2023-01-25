139b1c1d304323290a286f9eef9d813e1cb4fba4
Hazrat-Ali7 <programmer.hazratali@gmail.com>
2023-01-25 21:37:39 +0600

    digit sum code added

---
+++ Digits Sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        unsigned int x;
+        scanf("%u",&x);
+        printf("%u\n",(x+1)/10);
+    }
+    return 0;
+}
+
+
