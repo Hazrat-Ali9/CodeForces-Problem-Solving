9857f26ada1b12c6696a20ca19b1449283c0b332
Hazrat-Ali7 <programmer.hazratali@gmail.com>
2023-02-12 22:51:12 +0600

    Two Groups Code added

--- Sum.c
+++ Sum.c
@@ -1,18 +1,18 @@
-#include<stdio.h>
-int main ()
+// Hazart Ali
+// University Of Scholars
+#include <stdio.h>
+int main()
 {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    for(int i=1;i<=n;i++)
+    for (int i = 1; i <= n; i++)
     {
-        int a,b,c;
-        scanf("%d %d %d",&a,&b,&c);
-        if(a+b+c==2*a || a+b+c==2*b || a+b+c==2*c)
+        int a, b, c;
+        scanf("%d %d %d", &a, &b, &c);
+        if (a + b + c == 2 * a || a + b + c == 2 * b || a + b + c == 2 * c)
             printf("YES\n");
         else
             printf("NO\n");
     }
-
 }
-
