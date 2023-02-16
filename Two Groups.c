5b31942ec8bb736d461fd169bb60a1f7df41371a
Hazrat-Ali7 <programmer.hazratali@gmail.com>
2023-02-16 23:07:00 +0600

    Three Doors Code added

--- Two Groups.c
+++ Two Groups.c
@@ -1,25 +1,25 @@
 // Hazart Ali
 // University Of Scholars
 #include <stdio.h>
-#include<math.h>
+#include <math.h>
 
 int main()
 {
     int t;
-    scanf("%d",&t);
-    while(t--)
+    scanf("%d", &t);
+    while (t--)
     {
         int n;
-    scanf("%d",&n);
-    long int A[n],positive=0,negetive=0;
-    for(int i=0;i<n;i++)
-    {
-        scanf("%lld",&A[i]);
-        if(A[i]>=0)
-            positive+=A[i];
-        else
-            negetive+=A[i];
-    }
-    printf("%lld\n",abs(positive+negetive));
+        scanf("%d", &n);
+        long int A[n], positive = 0, negetive = 0;
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%lld", &A[i]);
+            if (A[i] >= 0)
+                positive += A[i];
+            else
+                negetive += A[i];
+        }
+        printf("%lld\n", abs(positive + negetive));
     }
 }
