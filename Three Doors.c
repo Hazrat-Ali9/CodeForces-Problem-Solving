cf598404bbf4cc78a515f3a914d5b1dc39001c5a
Hazrat-Ali7 <programmer.hazratali@gmail.com>
2023-02-20 21:55:21 +0600

    Winner Code added

--- Three Doors.c
+++ Three Doors.c
@@ -1,16 +1,18 @@
-#include<stdio.h>
+// Hazart Ali
+// University Of Scholars
+#include <stdio.h>
 void solve()
 {
     int key, d[3];
-    scanf("%d%d%d%d",&key,&d[0],&d[1],&d[2]);
-    for(int i=0;i<3;i++)
+    scanf("%d%d%d%d", &key, &d[0], &d[1], &d[2]);
+    for (int i = 0; i < 3; i++)
     {
-        int temp=d[key-1];
-        d[key-1]=5;
-        key=temp;
+        int temp = d[key - 1];
+        d[key - 1] = 5;
+        key = temp;
     }
 
-    if(d[0]==5 && d[1]==5 && d[2]==5)
+    if (d[0] == 5 && d[1] == 5 && d[2] == 5)
         printf("YES\n");
     else
         printf("NO\n");
@@ -19,10 +21,9 @@ void solve()
 int main(void)
 {
     int t;
-    scanf("%d",&t);
-    while(t--)
+    scanf("%d", &t);
+    while (t--)
     {
         solve();
     }
-
 }
