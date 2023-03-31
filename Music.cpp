0067f25a041487922ff81f407cda0333f70fcf9b
Hazrat-Ali7 <programmer.hazratali@gmail.com>
2023-03-31 19:56:26 +0600

    Music program added

---
+++ Music.cpp
@@ -0,0 +1,20 @@
+// Hazart Ali
+// University Of Scholars
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long int s,q,t,y,n,m;
+    while(cin>>t>>s>>q)
+    {
+        long ans=s;
+         m=0;
+        while(ans<t)
+        {
+            ans*=q;
+            m++;
+        }
+        cout<<m<<endl;
+    }
+    return 0;
+}
