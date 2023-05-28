71e14ebaec878fbcd6f258e55f068a18ca616eb0
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2023-05-28 23:58:53 +0600

    Duff in Love :ok_hand:

---
+++ Wizards  Duel.cpp
@@ -0,0 +1,15 @@
+// Hazart Ali
+// University Of Scholars
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double x,ans,a,b,spd;
+    while(cin>>x>>a>>b)
+    {
+        spd=x/(a+b);
+        ans=spd*a;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
