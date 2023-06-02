bf051ffe577365631f326e77199f45c4d042571a
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2023-06-02 22:53:29 +0600

    Hit The Lottery Program added :lock:

---
+++ Hit The Lottery.cpp
@@ -0,0 +1,17 @@
+// Hazart Ali
+// University Of Scholars
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,ans=0;
+    int a[]={25,20,10,5,1};
+    cin>>n;
+    for(int i=0; i<5; i++)
+    {
+        ans+=n/a[i];
+        n=n%a[i];
+    }
+    cout<<ans<<endl;
+    return 0;
+}
