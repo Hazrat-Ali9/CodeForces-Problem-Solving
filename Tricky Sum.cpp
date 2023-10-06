172a1629f300c8195baa77bcd4afd13346644227
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2023-10-06 21:40:47 +0600

    Tricky Sum program added:bulb:

---
+++ Tricky Sum.cpp
@@ -0,0 +1,25 @@
+// Hazart Ali
+// University Of Scholars
+#include<bits/stdc++.h>
+using  namespace std;
+int main()
+{
+    long long i,j,t,total,sum,n;
+    while(cin>>t)
+    {
+
+        while(t--)
+        {
+
+            cin>>n;
+            total=(n*(n+1))/2;
+            sum=total;
+            for(i=1;i<=n;i=2*i)
+            {
+                sum-=2*i;
+            }
+            cout<<sum<<endl;
+        }
+    }
+    return 0;
+}
