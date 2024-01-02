25d88c99f278745f47a02723a821f94a2c6ccf75
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-01-02 21:39:52 +0600

    Infinite Sequence :fire:

---
+++ Infinite Sequence.cpp
@@ -0,0 +1,20 @@
+// Hazart Ali
+// University Of Scholars
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long i,n,m;
+    while(cin>>n){
+            m=n;
+            for(i=1;i<n;i++){
+
+                if(m<=i)
+                break;
+                m=m-i;
+
+            }
+        cout<<m<<endl;
+    }
+    return 0;
+}
