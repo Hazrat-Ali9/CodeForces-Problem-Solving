94a6a7bfe56aa16e3dba13353d90a0ba05278499
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-02-18 22:50:47 +0600

    Timofey And Cubes :books:

---
+++ Timofey and cubes.cpp
@@ -0,0 +1,27 @@
+// Hazart Ali
+// University Of Scholars
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ll i,j,x,y,n;
+    while(cin>>n){
+            ll a[n+2];
+        for(i=1;i<=n;i++){
+            cin>>a[i];
+        }
+        i=1;
+        while(i<=n/2){
+            if(i%2){
+                swap(a[i],a[n-i+1]);
+            }
+            i++;
+        }
+        for(i=1;i<=n;i++)
+            cout<<a[i]<<" ";
+    }
+    return 0;
+}
+
