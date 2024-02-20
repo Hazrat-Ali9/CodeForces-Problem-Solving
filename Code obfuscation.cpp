3d97d8d414f5fb51e60fa7cec756318876576f82
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-02-20 23:19:32 +0600

    Code obfuscation :lock:

---
+++ Code obfuscation.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ll i,j,n,m;
+    string s;
+    while(cin>>s){
+            map<char,ll>m;
+            vector<char>v;
+        for(i=0;i<s.size();i++){
+           if(m[s[i]]==0){
+            v.push_back(s[i]);
+            m[s[i]]++;
+           }
+        }
+       ll x='a';
+        ll cnt=0;
+        for(i=0;i<v.size();i++){
+            if(v[i]==x){
+                x++;
+                cnt++;
+            }
+           else if(v[i]>x){
+                break;
+            }
+        }
+        if(cnt==v.size())cout<<"YES\n";
+        else
+            cout<<"NO\n";
+    }
+    return 0;
+}
