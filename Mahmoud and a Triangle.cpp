b06741fe2a141ecea2f759ae7e8c21c928a4a545
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-02-27 23:53:44 +0600

    Mahmoud and a Triangle :fire:

---
+++ Mahmoud and a Triangle.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ll i,j,n,m;
+    while(cin>>n){
+        ll a[n+1];
+        for(i=0;i<n;i++){
+            cin>>a[i];
+        }
+        sort(a,a+n);
+        bool f=0;
+        for(i=0;i<n-2;i++){
+            if(a[i]+a[i+1]>a[i+2])
+                {f=1;break;}
+        }
+        if(f)
+            cout<<"YES\n";
+        else
+            cout<<"NO\n";
+    }
+    return 0;
+}
