ed452f0f7bcc68616b84c10b0a81832bfcd19e58
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-05-20 21:40:28 +0600

    Broken Keyword :books:

---
+++ Broken Keyboard.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void solve_it();
+int main(void)
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+        solve_it();
+    return 0;
+}
+
+void solve_it()
+{
+    int n;
+    scanf("%d",&n);
+    int mod=n%3;
+    char str[n+1];
+    scanf("%s",str);
+
+    if(mod==2)
+    {
+        printf("NO\n");
+        return;
+    }
+
+    for(int i=0;i<n-mod;i+=3)
+    {
+        if(str[i+1]!=str[i+2])
+        {
+            printf("NO\n");
+            return;
+        }
+    }
+    printf("YES\n");
+}
+
