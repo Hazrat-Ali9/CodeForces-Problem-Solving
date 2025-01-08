7481ec47b72ae0f934e6e9ca234ca8abcd2e5b4e
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-01-08 12:08:19 +0600

    I wanna be the guy  🤡

--- I Wanna Be the Guy.c
+++ I Wanna Be the Guy.c
@@ -1,41 +1,26 @@
-#include<stdio.h>
-int main()
-{
-    int n,p=0,q=0;
-    scanf("%d",&n);
-    int A[n+1];
-    for(int i=0; i<=n; i++)
-        A[i]=0;
-    for(int i=0; i<=p; i++)
-    {
-        int x;
-        scanf("%d",&x);
-        if(i==0)
-        {
-            p=x;
-            continue;
-        }
-        A[x]=1;
-    }
-    for(int i=0; i<=q; i++)
-    {
-        int x;
-        scanf("%d",&x);
-        if(i==0)
-        {
-            q=x;
-            continue;
-        }
-        A[x]=1;
-    }
-    for(int i=1; i<=n; i++)
-    {
-        if(A[i]==0)
-        {
-            printf("Oh, my keyboard!");
-            return 0;
-        }
-    }
-    printf("I become the guy.");
-    return 0;
+#include <iostream>
+#include<set>
+using namespace std;
+int main() {
+	 int n;
+	 cin>>n;
+	 set<int> levels;
+	  int q,y;
+	  cin>>q;
+	  for(int i=0;i<q;i++){
+		  int x;
+		  cin>>x;
+		  levels.insert(x);
+	  }
+	  cin>>y;
+	  for(int i=0;i<y;i++){
+		  int w;
+		  cin>>w;
+		  levels.insert(w);
+	  }
+	  if(levels.size()==n)
+		  cout<<"I become the guy.";
+	  else
+		  cout<<"Oh, my keyboard!";
+	return 0;
 }
