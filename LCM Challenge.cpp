df67981bf6fcf918bcb75ae4c093b8423f422955
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-02-15 12:50:56 +0600

    LCM Challenge 🪀

---
+++ LCM Challenge.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long i,j,k,l,t,number,ans;
+    while(cin>>number)
+    {
+        if(number==1)
+            ans=1;
+        else if(number==2)
+            ans=2;
+        else if(number==3)
+            ans=6;
+        else if(number%2==0)
+        {
+            if(number%3==0)
+                ans=(number-1)*(number-2)*(number-3);
+            else
+                ans=number*(number-1)*(number-3);
+        }
+        else
+            ans=number*(number-1)*(number-2);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
