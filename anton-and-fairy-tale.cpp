be6d8113f88ab62c820bef89b3b6282259537cc4
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-04-11 16:03:57 +0600

    anton and fairy tale :tada:

--- anton-and-fairy-tale.cpp
+++ anton-and-fairy-tale.cpp
@@ -7,10 +7,10 @@ int main() {
   if (m >= n) {
     cout << n << endl;
   } else {
-    // double is not enough here
+    
     long double d = 1 + 8 * (n - m);
     long double days = (-1 + sqrt(d)) / 2;
-    // cast to long long before printing the answer
+    
     long long ans = ceil(days) + m;
     cout << ans << endl;
   }
