ce59bbed5245f7d2006e7d68f730db8a46a2b1a5
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-04-17 16:29:42 +0600

    Game With Sticks 🧒

---
+++ Game With Sticks.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define lli long long int
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+	int p,q;
+	cin>>p>>q;
+	if(min(p,q)%2 == 1)
+		cout<<"Akshat";
+	else
+ 		cout<<"Malvika";
+	return 0;
+}
