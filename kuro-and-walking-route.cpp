0b388bd1cb7edb294cf606d52cf9c81ac9155f61
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-05 22:07:27 +0600

    Kuro and Walking Route  :tada:

---
+++ kuro-and-walking-route.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  vector<vector<int>> cnn(a);
+  for (int i = 0; i < a - 1; i++) {
+    int x, y;
+    cin >> x >> y;
+    cnn[x - 1].push_back(y - 1);
+    cnn[y - 1].push_back(x - 1);
+  }
+  long long all = a * (a - 1);
+  long long no = cnn[b - 1].size() * cnn[c - 1].size();
+  cout << all - no << endl;
+  return 0;
+}
+
