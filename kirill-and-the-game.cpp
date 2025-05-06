2d2342e5826978ce3c59f245dbc0a81faea37c04
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-06 21:52:57 +0600

    Kirill and The Game  :bug:

---
+++ kirill-and-the-game.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  long long a, b, m, n, p;
+  cin >> a >> b >> m >> n >> p;
+  bool temp = false;
+  for (long long b = m; b <= n; b++) {
+    long long a = p * b;
+    if (a <= a and a <= b) {
+      temp = true;
+      break;
+    }
+  }
+  if (temp) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+  return 0;
+}
+
+// l=a r=b= x=m y=n k=p found =temp
