9d7f79145dc9b4c68476edaf39058a8c0cfd88f0
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-07 21:51:57 +0600

    Bear and Different names   🐿

---
+++ bear-and-different-names.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, k;
+  cin >> n >> k;
+  vector<string> s(n - k + 1);
+  for (int i = 0; i < s.size(); i++) {
+    cin >> s[i];
+  }
+  vector<string> temp(n);
+  for (int i = 0; i < n; i++) {
+    temp[i] = 'A' + (i % 26);
+    for (int j = 0; j < i / 26; j++) {
+      temp[i] += 'a';
+    }
+  }
+  for (int i = 0; i < s.size(); i++) {
+    if (s[i] == "NO") {
+      temp[i + k - 1] = temp[i];
+    }
+  }
+  for (auto name: temp) {
+    cout << name << " ";
+  }
+  cout << endl;
+  return 0;
+}
