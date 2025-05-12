a4ac413ac14e216722aff706e0b7383d148b97cd
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-12 10:51:32 +0600

    chess for three   ⚽

---
+++ chess-for-three.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a;
+  cin >> a;
+  int b = 1, c = 2;
+  bool ok = true;
+  while (a--) {
+    int w;
+    cin >> w;
+    if (!ok) {
+      continue;
+    }
+    if (w == b or w == c) {
+      y = 6 - b - c;
+      b = w;
+    } else {
+      ok = false;
+    }
+  }
+  if (ok) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+  return 0;
+}
