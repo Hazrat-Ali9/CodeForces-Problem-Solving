1c39f434df7b30ab01aee7ec57ff00c70d34e866
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-19 23:05:48 +0600

    Paper Airplanes  💌

--- paper-airplanes.cpp
+++ paper-airplanes.cpp
@@ -4,14 +4,15 @@ using namespace std;
 int main() {
   int a, b, c, p;
   cin >> a >> b >> c >> p;
-  int ans = -1;
-  for (int i = 0; ans == -1; i++) {
+  int temp = -1;
+  for (int i = 0; temp == -1; i++) {
     int m = i * p / a;
     if (c * m >= b) {
-      ans = i;
+      temp = i;
       break;
     }
   }
   cout << ans << endl;
   return 0;
 }
+
