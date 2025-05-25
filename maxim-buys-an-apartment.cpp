3c73ebcda411c15961b28105e6f3b21ab4913750
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-25 20:29:27 +0600

    Maxim buys an apartment  🚋

---
+++ maxim-buys-an-apartment.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  long long n, m;
+  cin >> n >> m;
+  if (m == 0 or m == n) {
+    cout << "0 0" << endl;
+    return 0;
+  }
+  long long mx = (n > 3 * m) ? 2 * m : n - m;
+  cout << 1 << " " << mx << endl;
+  return 0;
+}
