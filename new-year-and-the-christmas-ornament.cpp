13dc205f13941acf02c061c735fa6b2cfd426bed
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-27 19:29:29 +0600

    New Year and The Chrismas Ornament 🛼

---
+++ new-year-and-the-christmas-ornament.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  int temp;
+  for (int i = 1; i <= a; i++) {
+    bool ok = (i + 1) <= b and (i + 2) <= c;
+    if (ok) {
+      temp = 3 * i + 3;
+    }
+  }
+  cout << temp << endl;
+  return 0;
+}
