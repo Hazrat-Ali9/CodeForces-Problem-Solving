38cb0b2f5054636b285e074df5a701f3a5756c15
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-06-08 19:04:55 +0600

    Number on the chessboard  :rocket:

--- new-year-and-the-christmas-ornament.cpp
+++ new-year-and-the-christmas-ornament.cpp
@@ -1,16 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  int a, b, c;
-  cin >> a >> b >> c;
-  int temp;
-  for (int i = 1; i <= a; i++) {
-    bool ok = (i + 1) <= b and (i + 2) <= c;
-    if (ok) {
-      temp = 3 * i + 3;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    int temp;
+    for (int i = 1; i <= a; i++)
+    {
+        bool ok = (i + 1) <= b and (i + 2) <= c;
+        if (ok)
+        {
+            temp = 3 * i + 3;
+        }
     }
-  }
-  cout << temp << endl;
-  return 0;
+    cout << temp << endl;
+    return 0;
 }
