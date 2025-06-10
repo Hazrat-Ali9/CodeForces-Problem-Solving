43c8668f65c627af48a37484d35e242ffcc0cbb0
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-06-10 13:21:29 +0600

    cloning toys  🤯

---
+++ cloning-toys.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  long long x, y;
+  cin >> x >> y;
+  if (y == 0) {
+    cout << "No" << endl;
+    return 0;
+  }
+  long long originals = 1, copies = 0;
+  while (originals < y) {
+    originals++;
+    copies++;
+  }
+
+  while (copies > 0 and copies < x) {
+    copies += 2;
+  }
+  if (y == originals and x == copies) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+  return 0;
+}
