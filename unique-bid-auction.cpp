25afd18c0f08969c0379a6cbb9ff616da974626c
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-01 08:01:08 +0600

    umique bid auction 🚞

---
+++ unique-bid-auction.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        map<int, int> memo, pos;
+        for (int i = 0; i < n; i++)
+        {
+            int a;
+            cin >> a;
+            memo[a]++;
+            pos[a] = i;
+        }
+        int ans = -1;
+        for (auto [num, cnt]: memo)
+        {
+            if (cnt == 1)
+            {
+                ans = pos[num] + 1;
+                break;
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
