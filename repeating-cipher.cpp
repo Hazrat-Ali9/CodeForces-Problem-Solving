120eb01833180cd2ed20703d60c4b7ab6b64ac38
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-07 01:07:34 +0600

    Repeating Cipher 🛫

---
+++ repeating-cipher.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  string ans;
+  int temp = 0;
+  for (int i = 0; i < n; i += temp) {
+    ans += s[i];
+    temp++;
+  }
+  cout << ans << endl;
+  return 0;
+}
