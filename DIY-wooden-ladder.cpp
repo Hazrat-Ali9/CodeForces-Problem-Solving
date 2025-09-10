c7eabe124748b4441b7796c78fb944b0b180f8a2
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-09-10 11:55:54 +0600

    DIY wooden ladder :tshirt:

---
+++ DIY-wooden-ladder.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+      cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    int ans = (n > 2) ? min(n - 2, a[n - 2] - 1) : 0;
+    cout << ans << endl;
+  }
+  return 0;
+}
