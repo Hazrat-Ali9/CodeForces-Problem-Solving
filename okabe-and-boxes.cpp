0bd95bcb21c6506dedddecbb230537b13b2ce33f
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-09-15 20:08:20 +0600

    Okabe and boxes 🤮

---
+++ okabe-and-boxes.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    stack<int> s;
+    set<int> sorted;
+    int nxt = 1;
+    int ans = 0;
+    for (int i = 0; i < 2 * n; i++)
+    {
+        string cmd;
+        cin >> cmd;
+        if (cmd == "add")
+        {
+            int x;
+            cin >> x;
+            s.push(x);
+        }
+        else
+        {
+            if (s.empty())
+            {
+                sorted.erase(nxt);
+            }
+            else
+            {
+                if (s.top() == nxt)
+                {
+                    s.pop();
+                }
+                else
+                {
+                    while (!s.empty())
+                    {
+                        sorted.insert(s.top());
+                        s.pop();
+                    }
+                    sorted.erase(nxt);
+                    ans++;
+                }
+            }
+            nxt++;
+        }
+    }
+    cout << ans << "\n";
+    return 0;
+}
