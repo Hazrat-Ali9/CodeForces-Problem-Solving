2917dee6e61089b96d9352e7c66d68047709e1c2
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-11-08 12:17:42 +0600

    Treasure hunt :rocket:

--- div-times-mod.cpp
+++ div-times-mod.cpp
@@ -1,23 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-// Div Times Mod
-int main() {
-  int n, k;
-  cin >> n >> k;
-  vector<int> ans;
-  for (int i = 1; i < k; i++) {
-    if (n % i == 0) {
-      int x = i;
-      while (true) {
-        if (x / k * (x % k) == n) {
-          ans.push_back(x);
-          break;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> ans;
+    for (int i = 1; i < k; i++)
+    {
+        if (n % i == 0)
+        {
+            int x = i;
+            while (true)
+            {
+                if (x / k * (x % k) == n)
+                {
+                    ans.push_back(x);
+                    break;
+                }
+                x += k;
+            }
         }
-        x += k;
-      }
     }
-  }
-  sort(ans.begin(), ans.end());
-  cout << ans[0] << endl; 
-  return 0;
+    sort(ans.begin(), ans.end());
+    cout << ans[0] << endl;
+    return 0;
 }
