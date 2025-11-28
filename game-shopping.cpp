235348a92c65e20c5e51af61c4a2e5a62e8c3783
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-11-28 22:26:44 +0600

    Reorder-the-array  :tshirt:

--- game-shopping.cpp
+++ game-shopping.cpp
@@ -1,30 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  int n, m;
-  cin >> n >> m;
-  vector<int> c(n);
-  for (int i = 0; i < n; i++) {
-    cin >> c[i];
-  }
-  queue<int> q;
-  while (m--) {
-    int a;
-    cin >> a;
-    q.push(a);
-  }
-  int ans = 0;
-  for (int i = 0; i < n; i++) {
-    if (q.empty()) {
-      break;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> c(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> c[i];
     }
-    int front = q.front();
-    if (front >= c[i]) {
-      q.pop();
-      ans++;
+    queue<int> q;
+    while (m--)
+    {
+        int a;
+        cin >> a;
+        q.push(a);
     }
-  }
-  cout << ans << "\n";
-  return 0;
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (q.empty())
+        {
+            break;
+        }
+        int front = q.front();
+        if (front >= c[i])
+        {
+            q.pop();
+            ans++;
+        }
+    }
+    cout << ans << "\n";
+    return 0;
 }
