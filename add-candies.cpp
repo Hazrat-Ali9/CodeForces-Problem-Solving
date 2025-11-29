2b5c477471ba4e62b554153ff0dc6d69a23ad653
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-11-29 22:03:03 +0600

    Bad Prices :tshirt:

--- add-candies.cpp
+++ add-candies.cpp
@@ -1,19 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  int t;
-  cin >> t;
-  while (t--) {
-    int n;
-    cin >> n;
-    cout << n - 1 << endl;
-    for (int i = 2; i <= n; i++) {
-      cout << i << " ";
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        cout << n - 1 << endl;
+        for (int i = 2; i <= n; i++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
-  return 0;
+    return 0;
 }
--- floor-number.cpp
+++ floor-number.cpp
@@ -1,19 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  int t;
-  cin >> t;
-  while (t--) {
-    int n, x;
-    cin >> n >> x;
-    int f = 0;
-    while (n > f * x + 2) {
-      f++;
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x;
+        cin >> n >> x;
+        int f = 0;
+        while (n > f * x + 2)
+        {
+            f++;
+        }
+        cout << f + 1 << endl;
     }
-    cout << f + 1 << endl;
-  }
-  return 0;
+    return 0;
 }
