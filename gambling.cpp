0c62972a2a92c494ea31afbf452424cbaf124c82
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-12-08 16:52:50 +0600

    Minimizing the String  :tada:

--- gambling.cpp
+++ gambling.cpp
@@ -1,40 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
-int main() {
-  int n;
-  cin >> n;
-  vector<int> a(n);
-  for (int i = 0; i < n; i++) {
-    cin >> a[i];
-  }
-  sort(a.rbegin(), a.rend());
-  vector<int> b(n);
-  for (int i = 0; i < n; i++) {
-    cin >> b[i];
-  }
-  sort(b.rbegin(), b.rend());
-  long long sa = 0, sb = 0;
-  int i = 0, j = 0;
-  int turn = 0;
-  while (i != n or j != n) {
-    if (turn % 2 == 0) {
-      if (j == n or (i < n and a[i] > b[j])) {
-        sa += a[i];
-        i = min(n, i + 1);
-      } else {
-        j = min(n, j + 1);
-      }
-    } else {
-      if (i == n or (j < n and b[j] > a[i])) {
-        sb += b[j];
-        j = min(n, j + 1);
-      } else {
-        i = min(n, i + 1);
-      }
+int main()
+{
+   
+    int T;
+    if(!(cin >> T)) return 0;
+    while (T--)
+    {
+        int n;
+        cin >> n;
+        vector<int> x(n+1);
+        for (int i = 1; i <= n; ++i) cin >> x[i];
+
+        unordered_map<int, vector<int>> pos;
+        pos.reserve(n*2);
+        for (int i = 1; i <= n; ++i) pos[x[i]].push_back(i);
+
+        ll bestVal = LLONG_MIN;
+        int besta = 1, bestl = 1, bestr = 1;
+
+        for (auto &entry : pos)
+        {
+            int a = entry.first;
+            vector<int> &v = entry.second;
+
+            ll cur = 1;
+            int curL = v[0];
+
+            if (cur > bestVal)
+            {
+                bestVal = cur;
+                besta = a;
+                bestl = curL;
+                bestr = v[0];
+            }
+            for (size_t i = 1; i < v.size(); ++i)
+            {
+                int gap = v[i] - v[i-1];
+                ll delta = 2 - gap;
+                if (cur + delta < 1)
+                {
+
+                    cur = 1;
+                    curL = v[i];
+                }
+                else
+                {
+                    cur += delta;
+                }
+                if (cur > bestVal)
+                {
+                    bestVal = cur;
+                    besta = a;
+                    bestl = curL;
+                    bestr = v[i];
+                }
+            }
+        }
+
+        cout << besta << ' ' << bestl << ' ' << bestr << '\n';
     }
-    turn++;
-  }
-  cout << sa - sb << endl;
-  return 0;
+    return 0;
 }
