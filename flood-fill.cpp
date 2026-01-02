1bf4ef93a3c21c3055244d7d1d1a817beed0ff7a
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-01-02 19:31:29 +0600

    RGB Substring (easy version) RGB Substring (easy version) ☎

--- flood-fill.cpp
+++ flood-fill.cpp
@@ -1,39 +1,45 @@
-// WA
 #include <bits/stdc++.h>
 #define MAXN 5005
 using namespace std;
 
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  int n;
-  cin >> n;
-  vector<int> c(n);
-  for (int i = 0; i < n; i++) {
-    cin >> c[i];
-  }
-  vector<int> diff;
-  diff.push_back(c[0]);
-  for (int i = 1; i < n; i++) {
-    if (c[i] != c[i - 1]) {
-      diff.push_back(c[i]);
+int main()
+{
+
+    int n;
+    cin >> n;
+    vector<int> c(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> c[i];
     }
-  }
-  int mf = -1, mn = -1;
-  vector<int> f(MAXN);
-  for (auto val: diff) {
-    f[val]++;
-    if (f[val] > mf) {
-      mf = f[val];
-      mn = val;
+    vector<int> diff;
+    diff.push_back(c[0]);
+    for (int i = 1; i < n; i++)
+    {
+        if (c[i] != c[i - 1])
+        {
+            diff.push_back(c[i]);
+        }
     }
-  }
-  int ans = 0;
-  for (auto val: diff) {
-    if (val != mn) {
-      ans++;
+    int mf = -1, mn = -1;
+    vector<int> f(MAXN);
+    for (auto val: diff)
+    {
+        f[val]++;
+        if (f[val] > mf)
+        {
+            mf = f[val];
+            mn = val;
+        }
     }
-  }
-  cout << ans << endl;
-  return 0;
+    int ans = 0;
+    for (auto val: diff)
+    {
+        if (val != mn)
+        {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+    return 0;
 }
