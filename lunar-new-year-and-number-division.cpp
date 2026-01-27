ab76ca52fd26a3ed890ee3e61dbed624f130fef9
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-01-27 23:10:03 +0600

    Lunar New Year and Number Division :books:

---
+++ lunar-new-year-and-number-division.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    vector<long long> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    long long ans = 0;
+    sort(a.begin(), a.end());
+    for (int i = 0; i < n / 2; i++)
+    {
+        ans += (a[i] + a[n - 1 - i]) * (a[i] + a[n - 1 - i]);
+    }
+    cout << ans << endl;
+    return 0;
+}
