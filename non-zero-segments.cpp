7c017068c8d2083eb288eac531c9877cfc5fab87
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-01-29 23:11:33 +0600

    Vasya and Book  :books:

--- non-zero-segments.cpp
+++ non-zero-segments.cpp
@@ -1,29 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  int n;
-  cin >> n;
-  vector<int> a(n);
-  for (int i = 0; i < n; i++) {
-    cin >> a[i];
-  }
-  int ans = 0;
-  long long ps = 0;
-  set<long long> s;
-  s.insert(0);
-  for (int i = 0; i < n; i++) {
-    ps += a[i];
-    if (s.count(ps) > 0) {
-      s.clear();
-      s.insert(0);
-      ps = a[i];
-      ans++;
+int main()
+{
+
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
     }
-    s.insert(ps);
-  }
-  cout << ans << endl;
-  return 0;
+    int ans = 0;
+    long long ps = 0;
+    set<long long> s;
+    s.insert(0);
+    for (int i = 0; i < n; i++)
+    {
+        ps += a[i];
+        if (s.count(ps) > 0)
+        {
+            s.clear();
+            s.insert(0);
+            ps = a[i];
+            ans++;
+        }
+        s.insert(ps);
+    }
+    cout << ans << endl;
+    return 0;
 }
