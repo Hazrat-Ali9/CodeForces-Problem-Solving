b6bbd91273b9de11c74db9e8a21ef3deab41bb2f
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-01-31 20:02:10 +0600

    Cutting Out :books:

--- smallest-word.cpp
+++ smallest-word.cpp
@@ -1,21 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  string s;
-  cin >> s;
-  int n = s.size();
-  vector<int> flip(n);
-  for (int i = 0; i < n; i++) {
-    if (i == n - 1) {
-      flip[i] = s[i] == 'a' ? 1 : 0;
-    } else {
-      flip[i] = s[i] != s[i + 1] ? 1 : 0; 
+int main()
+{
+    string s;
+    cin >> s;
+    int n = s.size();
+    vector<int> temp(n);
+    for (int i = 0; i < n; i++)
+    {
+        if (i == n - 1)
+        {
+            temp[i] = s[i] == 'a' ? 1 : 0;
+        }
+        else
+        {
+            temp[i] = s[i] != s[i + 1] ? 1 : 0;
+        }
     }
-  }
-  for (int i = 0; i < n; i++) {
-    cout << flip[i] << " ";
-  }
-  cout << endl;
-  return 0;
+    for (int i = 0; i < n; i++)
+    {
+        cout << temp[i] << " ";
+    }
+    cout << endl;
+    return 0;
 }
