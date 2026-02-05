257a91904ce0e7c03fc728c50777945b11c02823
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-02-05 16:57:05 +0600

    Longest Strike 😺

--- almost-equal.cpp
+++ almost-equal.cpp
@@ -1,27 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  int n;
-  cin >> n;
-  vector<int> p(2 * n);
-  for (int i = 0; i < n; i++) {
-    p[i] = 2 * i;
-    p[i + n] = 2 * i + 1;
-    if (i % 2 == 1) {
-      swap(p[i], p[i + n]);
+int main()
+{
+
+    int n;
+    cin >> n;
+    vector<int> p(2 * n);
+    for (int i = 0; i < n; i++)
+    {
+        p[i] = 2 * i;
+        p[i + n] = 2 * i + 1;
+        if (i % 2 == 1)
+        {
+            swap(p[i], p[i + n]);
+        }
     }
-  }
-  if (n % 2 == 1) {
-    cout << "YES" << endl;
-    for (auto pi: p) {
-      cout << pi + 1 << " ";
+    if (n % 2 == 1)
+    {
+        cout << "YES" << endl;
+        for (auto pi: p)
+        {
+            cout << pi + 1 << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
-  } else {
-    cout << "NO" << endl;
-  }
-  return 0;
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
 }
